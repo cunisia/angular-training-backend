001c3eeeb474b5bd8e3eefc70c71a7c9584f24b6
Pierre Cuni <pierre.cuni@akur8.com>
2025-03-28 11:18:00 +0100

    feat: accept cors requests

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+
+// Enable CORS for requests from everywhere
+app.use(cors());
 
 const housingLocations = [
   {
